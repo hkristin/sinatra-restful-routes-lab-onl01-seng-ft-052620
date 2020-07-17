061add81eb382d45014d2983aa13be163c57917c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-17 02:14:03 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,15 @@ set :views, Proc.new { File.join(root, "../views/") }
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  get 'recipes/new' do
+  erb :new
+  end
   
   get '/recipes' do
     @recipes = Recipe.all
     erb :index
   end
   
-  get 'recipes/new' do
-    erb :new
-  end
    post '/recipes' do 
     @recipe = Recipe.create(params)
     redirect to '/recipes/:id'
