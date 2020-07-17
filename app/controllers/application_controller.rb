a13969f488d76c667a6f67e9878b8b0cf8a306c1
Kristin Howard <hkristin@andrew.cmu.edu>
2020-07-17 02:22:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ set :views, Proc.new { File.join(root, "../views/") }
     set :views, 'app/views'
   end
   
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
   
   get 'recipes/new' do
     erb :new
@@ -17,11 +21,6 @@ set :views, Proc.new { File.join(root, "../views/") }
     @recipe = Recipe.create(params)
     redirect to '/recipes/:id'
    end
-   
-  get '/recipes' do
-    @recipes = Recipe.all
-    erb :index
-  end
   
   get '/recipes/:id' do
     @recipe = Recipe.find_by_id(params[:id])
